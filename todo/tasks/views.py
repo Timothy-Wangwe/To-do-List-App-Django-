876ff18090967fced3540bd748f058da8525b789
T-Skipper <wangwetymothy@gmail.com>
2020-05-04 22:01:16 +0300

    Test views

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def summary_view(request):
+    return HttpResponse("A summary of all tasks come here...")
+    
+def new_view(request):
+    return HttpResponse("New tasks come here...")
+
+
+def done_view(request):
+    return HttpResponse("Finished tasks come here...")
