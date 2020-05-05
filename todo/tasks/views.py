37263280a9b40e2f7e44e6f016c67d93a4cd309c
T-Skipper <wangwetymothy@gmail.com>
2020-05-06 00:59:14 +0300

    Display tasks as card from database

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tasks
 
-
-def summary_view(request):
-    # tasks = Tasks.objects.all()
-    return render(request, 'index.html')
+def summary_view(request): 
+    tasks = Tasks.objects.all()
+    return render(request, 'index.html', {'tasks': tasks})
     
 def new_view(request):
     return HttpResponse("New tasks come here...")
@@ -12,3 +12,4 @@ def new_view(request):
 
 def done_view(request):
     return HttpResponse("Finished tasks come here...")
+
