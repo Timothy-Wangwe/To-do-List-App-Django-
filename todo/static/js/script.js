596491166b223b51fdd7c75cce3a2b4275b71331
T-Skipper <wangwetymothy@gmail.com>
2020-05-06 04:23:12 +0300

    Customized form and buttons for new task

--- todo/static/js/script.js
+++ todo/static/js/script.js
@@ -2,6 +2,16 @@
 function new_task(event, input) {
 
     document.getElementById(input).style.display = "inline";
+    document.getElementById('add').style.display = "none";
+    document.getElementById('cancel').style.display = "inline";
+}
+
+// Hide form for new task
+function cancel_new_task(event, input) {
+
+    document.getElementById(input).style.display = "none";
+    document.getElementById('add').style.display = "inline";
+    document.getElementById('cancel').style.display = "none";
 }
 
 var date = document.getElementById('due-date');
