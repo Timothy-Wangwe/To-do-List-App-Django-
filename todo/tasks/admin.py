b54b8a7e6cb8b56c275d6a1a6362e77232472c85
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-15 11:54:40 +0300

    Remove everything, get ready for a new start

--- todo/tasks/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tasks
-
-class TasksAdmin(admin.ModelAdmin):
-    list_display = ('date', 'subject', 'description')
-
-admin.site.register(Tasks, TasksAdmin)
--- todo/tasks/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Tasks(models.Model):
-    date = models.DateTimeField(auto_now=False, null = False)
-    subject = models.CharField(max_length = 200)
-    description = models.CharField(max_length = 2000)
