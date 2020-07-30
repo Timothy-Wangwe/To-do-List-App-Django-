3e86892a37dd30ca560a9c0e8971456837671d78
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-30 22:03:29 +0300

    Create input forms for tasks and task lists

--- task/forms.py
+++ task/forms.py
@@ -6,3 +6,20 @@ def widget_attrs(placeholder):
 def form_kwargs(widget, label, max_length):
 	return {'widget':widget, 'label': label, 'max_length': max_length}
 
+class TaskListForm(forms.Form):
+	title = forms.CharField(
+		**form_kwargs(
+			widget = forms.TextInput(attrs=widget_attrs('Enter the title of your new list...')),
+			label='',
+			max_length=20
+			)
+		)
+
+class TaskForm(forms.Form):
+	description = forms.CharField(
+		**form_kwargs(
+			widget = forms.TextInput(attrs=widget_attrs('Description of your task...')),
+			label='',
+			max_length=255
+			)
+		)
