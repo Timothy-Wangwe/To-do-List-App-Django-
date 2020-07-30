0123c519462ea1af4dd3c6f5989b27ed773a288b
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-30 22:08:55 +0300

    Route url to task list and task display

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, task_list, task_display
 
 app_name = 'task'
 urlpatterns = [
 	path('', index, name='index'),
+	path('lists/', task_list, name='task_list'),
+	path('lists/<int:pk>/', task_display, name='task_display'),
 ]
