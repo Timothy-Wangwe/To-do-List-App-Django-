1e7e928c4d72234e53c87da45c5e524722bfd5f1
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-30 21:59:20 +0300

    Register models to admin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from task.models import Task, TaskList
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('description', 'created', 'is_complete')
+
+class TaskListAdmin(admin.ModelAdmin):
+	list_display = ('title', 'created')
+
+admin.site.register(TaskList, TaskListAdmin)
+admin.site.register(Task, TaskAdmin)
