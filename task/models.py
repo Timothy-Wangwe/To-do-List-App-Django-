081f4b11fb3672eec9296e5e807a53cb592adcd5
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-30 22:06:50 +0300

    Update task list model, created task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class TaskList(models.Model):
+	title = models.CharField(max_length=20)
+	created = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ('created',)
+
+	def __str__(self):
+		return self.title
+
+	def count(self):
+		return self.tasks.count()
+
+	def count_complete(self):
+		return self.tasks.filter(is_complete=True).count()
+
+	def count_incomplete(self):
+		return self.tasks.filter(is_complete=False).count()
+
+
+class Task(models.Model):
+	tasklist = models.ForeignKey(TaskList, related_name='tasks', on_delete=models.CASCADE)
+	description = models.CharField(max_length=255)
+	created = models.DateTimeField(auto_now=True)
+	completed_at = models.DateTimeField(null=True)
+	is_complete = models.BooleanField(default=False)
+
+	class Meta:
+		ordering = ('tasklist', 'created', 'completed_at')
+
+	def __str__(self):
+		return self.description
+
+	def complete(self):
+		self.is_complete = True
+		self.completed_at = timezone.now()
+		self.save()
+
+	def restart(self):
+		self.is_complete = False
+		self.completed_at = None
+		self.save()
