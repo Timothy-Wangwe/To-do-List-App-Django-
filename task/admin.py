99caf85e9b210de7c6787fbdd6c210eee6e6fc7f
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-31 10:29:40 +0300

    Display tasks inline:tasklist

--- task/admin.py
+++ task/admin.py
@@ -5,8 +5,12 @@ from task.models import Task, TaskList
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('description', 'created', 'is_complete')
 
+class TaskInline(admin.TabularInline):
+	model = Task
+
 class TaskListAdmin(admin.ModelAdmin):
 	list_display = ('title', 'created')
+	inlines = [TaskInline]
 
 admin.site.register(TaskList, TaskListAdmin)
 admin.site.register(Task, TaskAdmin)
